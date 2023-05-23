15fc4a1a1d999e4eff52d4a441e8f5a662f70ce0
alpsertel <alparslansertel17@gmail.com>
2023-05-23 20:28:00 +0300

    Update utils.py
    added data_organizer (pulls retention from toyshop data), and revenue_estimator

--- utils.py
+++ utils.py
@@ -123,4 +123,89 @@ def b_calc(p, a, ms, T, ltv, n, disc):
 
 def g_16(a, q, f_integral):
     return ((a + 1) * (1 - q)) / ((1 + a * q) * np.exp((1 + a) * (f_integral)) + a * (1 - q))
+
+def data_organizer(df): #toyshop data
+    df = df.drop(['eCPI', 'Imp.(UA)', 'eCPM(UA)', 'Profit', 'ROI', 'Paying Users', 'IAP Count', 'Last Day'], axis=1)
+    df = df.fillna(method='ffill')
+    df = df.replace('[\$]', '', regex=True)
+    df = df.replace('[\%]', '', regex=True)
+    df = df.replace('[\.]', '', regex=True)
+    df = df.replace(',', '.', regex=True)
     
+    arpu_index = []
+    n = 0
+    for i in range(int(len(df)/3)):
+        arpu_index.append(3*i)
+        n = n+1
+    
+    retention_index = arpu_index
+    retention_index = [index + 1 for index in arpu_index]
+    users_index = [index + 2 for index in arpu_index]
+    
+    df_arpu = df.iloc[arpu_index]
+    df_arpu['Install Day'] = df_arpu['Install Day'].str.replace('ARPU: ', '')
+    df_arpu.iloc[:, 1:] = df_arpu.iloc[:, 1:].astype(float)
+    df_arpu = df_arpu.tail(-1).reset_index(drop=True)
+    
+    df_retention = df.iloc[retention_index]
+    df_retention['Install Day'] = df_retention['Install Day'].str.replace('Retention: ', '')
+    df_retention.iloc[:, 1:] = df_retention.iloc[:, 1:].astype(float)
+    df_retention = df_retention.tail(-1).reset_index(drop=True)
+    
+    df_users = df.iloc[users_index]
+    df_users['Install Day'] = df_users['Install Day'].str.replace('Users: ', '')
+    df_users.iloc[:, 1:] = df_users.iloc[:, 1:].astype(float)
+    df_users = df_users.tail(-1).reset_index(drop=True)
+    
+    return df_retention
+    
+def revenue_estimator(df, p, n, T, B, b):
+    
+    df = data_organizer(df)
+    df = df.drop(['Unnamed: 0'], axis=1)
+
+    df['active_users'] = df['Install']
+    
+    avg = df.mean()
+    avg = avg.drop(["Install", "Cost", "Revenue", "Install Day", "Day 14", "Day 21", "Day 30", "Day 45", "Day 60", "Day 90", "active_users"])
+    
+    y = df['Revenue']
+    x = df[['Cost','active_users']]
+     
+    y_norm = (y-y.min())/(y.max()-y.min())
+    x_norm = (x-x.min())/(x.max()-x.min())
+    
+    ## Dummy df for estimated campaign 
+    
+    # In here, we will take the values of b, p, and audience size (n) as input.
+    # Then, we will calculate estimated active users for each day of the campaign.
+    # Using active_users and cost as parameters, we will estimate revenue for each
+    # day using linear regression. We will cumsum daily revenues to find final 
+    # estimated total revenue.
+
+    model = LinearRegression().fit(x_norm,y_norm)
+    
+    df2 = {'Install': p*n/7, 'Cost': b, 'active_users': 0}
+    
+    for i in range(T):
+        df = df.append(df2, ignore_index = True)
+        
+    # calculate estimated active users
+    for i in range(len(df)-7):
+        i = i + 7
+        sum_temp = 0
+        for j in range(len(avg)):
+            sum_temp += avg.iloc[j]*df["Install"].iloc[i-j-1]/100
+        df["active_users"].iloc[i] = sum_temp
+        sum_temp = 0
+    
+    campaign_data = df[['Cost','active_users']].tail(T)
+    
+    campaign_data_norm = (campaign_data-campaign_data.min())/(campaign_data.max()-campaign_data.min())
+    b = model.predict(campaign_data) # not using the normalized data, will be reviewed later
+    b_unnorm = b*(y.max()-y.min()) + y.min()
+    
+    total_revenue = b.sum()
+    estimated_profit = total_revenue - B
+    
+    return estimated_profit
