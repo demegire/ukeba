4a9e9ccba48f2459bc10aed84f61fa2f65083668
Ege Demir <62503047+demegire@users.noreply.github.com>
2023-06-05 20:36:58 +0300

    Add online learning for different audiences

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ from pandas.core.common import SettingWithCopyWarning
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
-from flask import Flask, flash, render_template, request, redirect, url_for, session
-from utils import exponential_effectiveness, new_bid, pareto_frontier_B_b, maximize_p1p2_sum, maximize_n1n2_sum, maximize_ltv1ltv2_sum, revenue_estimator
+from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_ngrok import run_with_ngrok
+from utils import exponential_effectiveness, new_bid, pareto_frontier_B_b, maximize_ltv1ltv2_sum, revenue_estimator
 from multi import multi_pareto_frontier_B_b
 from werkzeug.utils import secure_filename
 from matplotlib.figure import Figure
@@ -25,6 +26,7 @@ ALLOWED_EXTENSIONS = {'csv', 'pdf', 'xlsx'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "super secret key"
+#run_with_ngrok(app)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -143,68 +145,68 @@ def rapor():
     graphJSON = plot_px(df)
     graphJSON2 = plot_pareto(pareto_df)    
 
-    return render_template('rapor.html', graphJSON=graphJSON, graphJSON2=graphJSON2, m=m, u=u, kitle='Etkililik Fonksiyonu R^2 Değeri: ' + str(r2))
+    return render_template('rapor.html', graphJSON=graphJSON, graphJSON2=graphJSON2, m=m, u=u, kitle='Etkililik Fonksiyonu R^2 Değeri: ' + "{:.2f}".format(r2))
 
 @app.route('/rapor_platform.html', methods =['GET', 'POST'])
 def rapor_platform():
-    df1 = pd.read_pickle("./Meta.pickle") 
-    
-    df1 = df1.sort_values(by=['Cost'])
-    df1['Verilen İhale Değeri'] = df1['Cost']
-    df1['Sonuç Yüzdesi'] = df1['Install'] / AUDIENCE_SIZE
-
-    cum_ad_spend_1 = np.array(df1['Verilen İhale Değeri'], dtype='f')
-    cum_result_1 = np.array(df1['Sonuç Yüzdesi'], dtype='f')
+    budgetJSON = ''
+    graphJSON1 = ''
+    graphJSON2 = ''
+    if request.method == 'POST' and request.form['Bütçe'] and request.form['Ironsource Gelir Çarpanı'] and request.form['Meta Gelir Çarpanı']:
+        df1 = pd.read_pickle("./Meta.pickle") 
         
-    popt_1, _ = curve_fit(exponential_effectiveness, cum_ad_spend_1, cum_result_1, p0=P0, maxfev=5000)
+        df1 = df1.sort_values(by=['Cost'])
+        df1['Verilen İhale Değeri'] = df1['Cost']
+        df1['Sonuç Yüzdesi'] = df1['Install'] / AUDIENCE_SIZE
 
-    df2 = pd.read_pickle("./Ironsource.pickle") 
-    
-    df2 = df2.sort_values(by=['Date'])
-    df2['Verilen İhale Değeri'] = df2['Cost']
-    df2['Sonuç Yüzdesi'] = df2['Install']  / AUDIENCE_SIZE
+        cum_ad_spend_1 = np.array(df1['Verilen İhale Değeri'], dtype='f')
+        cum_result_1 = np.array(df1['Sonuç Yüzdesi'], dtype='f')
+            
+        popt_1, _ = curve_fit(exponential_effectiveness, cum_ad_spend_1, cum_result_1, p0=P0, maxfev=5000)
 
-    cum_ad_spend_2 = np.array(df2['Verilen İhale Değeri'], dtype='f')
-    cum_result_2 = np.array(df2['Sonuç Yüzdesi'], dtype='f')
+        df2 = pd.read_pickle("./Ironsource.pickle") 
         
-    popt_2, _ = curve_fit(exponential_effectiveness, cum_ad_spend_2, cum_result_2, p0=P0, maxfev=5000)
+        df2 = df2.sort_values(by=['Date'])
+        df2['Verilen İhale Değeri'] = df2['Cost']
+        df2['Sonuç Yüzdesi'] = df2['Install']  / AUDIENCE_SIZE
 
-    b_limit = 5000 # take as input later
-    platform_pars_1 = [0.1, 400000, 19.28] # a,n,ltv - take as input
-    m_u_1 = popt_1
-    platform_pars_2 = [2, 2500, 6.06]
-    m_u_2 = popt_2
-    
-    ltv_results = maximize_ltv1ltv2_sum(platform_pars_1, m_u_1, platform_pars_2, m_u_2, b_limit=b_limit)    
-    
-    #  return actual_p1, actual_p2, actual_reach1, actual_reach2, actual_ltv1, actual_ltv2, opt_b1, opt_b2, max_sum
+        cum_ad_spend_2 = np.array(df2['Verilen İhale Değeri'], dtype='f')
+        cum_result_2 = np.array(df2['Sonuç Yüzdesi'], dtype='f')
+            
+        popt_2, _ = curve_fit(exponential_effectiveness, cum_ad_spend_2, cum_result_2, p0=P0, maxfev=5000)
 
-    names_pl = ['Meta', 'Ironsource']
-    p1, p2, h1, h2, _, _, B1, B2, _ = ltv_results
-    data = {
-    'budget_allocated': [B1, B2],
-    'exposure_percentage': [p1, p2],
-    'exposed_population': [h1*p1, h2*p2],
-    'names_platform': names_pl,
-    'unexposed_population': [h1*(1-p1), h2*(1-p2)],
-    'platform_1_exposed_unexposed':[h1*p1, h1*(1-p1)],
-    'platform_2_exposed_unexposed':[h2*p2, h2*(1-p2)],
-    'names_exposure': ['Exposed Population', 'Unexposed Population']
-       
-}
+        b_limit = float(request.form['Bütçe'])
+        platform_pars_1 = [WORD_OF_MOUTH, META_AUDIENCE_SIZE, float(request.form['Meta Gelir Çarpanı'])] # a,n,ltv - take as input
+        m_u_1 = popt_1
+        platform_pars_2 = [WORD_OF_MOUTH, IRONSOURCE_AUDIENCE_SIZE, float(request.form['Ironsource Gelir Çarpanı'])]
+        m_u_2 = popt_2
+        
+        ltv_results = maximize_ltv1ltv2_sum(platform_pars_1, m_u_1, platform_pars_2, m_u_2, b_limit=b_limit)    
+        
+        p1, p2, h1, h2, _, _, B1, B2, _ = ltv_results
+        data = {
+        'budget_allocated': [B1, B2],
+        'exposure_percentage': [p1, p2],
+        'exposed_population': [h1*p1, h2*p2],
+        'names_platform': ['Meta', 'Ironsource'],
+        'unexposed_population': [h1*(1-p1), h2*(1-p2)],
+        'platform_1_exposed_unexposed':[h1*p1, h1*(1-p1)],
+        'platform_2_exposed_unexposed':[h2*p2, h2*(1-p2)],
+        'names_exposure': ['Exposed Population', 'Unexposed Population']
+        }
 
-    df = pd.DataFrame(data)
+        df = pd.DataFrame(data)
 
-    fig = px.pie(df, values='budget_allocated', names='names_platform', title='Budgets to be allocated')
-    budgetJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        fig = px.pie(df, values='budget_allocated', names='names_platform', title='Budgets to be allocated')
+        budgetJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    # Pie for exposure in platform 1
-    fig = px.pie(df, values='platform_1_exposed_unexposed', names='names_exposure', title='Meta')
-    graphJSON1 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        # Pie for exposure in platform 1
+        fig = px.pie(df, values='platform_1_exposed_unexposed', names='names_exposure', title='Meta')
+        graphJSON1 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    # Pie for exposure in platform 2
-    fig = px.pie(df, values='platform_2_exposed_unexposed', names='names_exposure', title='Ironsource')
-    graphJSON2 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        # Pie for exposure in platform 2
+        fig = px.pie(df, values='platform_2_exposed_unexposed', names='names_exposure', title='Ironsource')
+        graphJSON2 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template('rapor_platform.html', budgetJSON=budgetJSON, graphJSON1=graphJSON1, graphJSON2=graphJSON2)
 
@@ -218,7 +220,7 @@ def rapor_kitle():
         return graphJSON
 
     def plot_pareto(df):
-        fig = px.line(df, x='B', y='T', color='P', markers=True, hover_data=['Bid Meta', 'Bid Ironsource'], title="B vs T vs P (Min B)")
+        fig = px.line(df, x='B', y='T', color='P', markers=True, hover_data=['Bid Meta', 'Bid Ironsource', 'P Meta', 'P Ironsource'], title="B vs T vs P (Min B)")
         graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
         
         return graphJSON
@@ -243,7 +245,7 @@ def rapor_kitle():
     ss_res = np.sum((cum_result_1 - exponential_effectiveness(cum_ad_spend_1, *popt_1)) ** 2)
     ss_tot = np.sum((cum_result_1 - np.mean(cum_result_1)) ** 2)
     r2 = 1 - (ss_res / ss_tot)
-    kitle += ('Meta Etkililik Fonksiyonu R^2 Değeri: ' + str(r2))
+    kitle += ('Meta Etkililik Fonksiyonu R^2 Değeri: ' + "{:.2f}".format(r2))
  
     df2 = pd.read_pickle("./Ironsource.pickle") 
     
@@ -264,7 +266,7 @@ def rapor_kitle():
     ss_res = np.sum((cum_result_2 - exponential_effectiveness(cum_ad_spend_2, *popt_2)) ** 2)
     ss_tot = np.sum((cum_result_2 - np.mean(cum_result_2)) ** 2)
     r2 = 1 - (ss_res / ss_tot)
-    kitle += ('\n\nIronsource Etkililik Fonksiyonu R^2 Değeri: ' + str(r2))
+    kitle += ('\n\nIronsource Etkililik Fonksiyonu R^2 Değeri: ' + "{:.2f}".format(r2))
 
     p_vals = [0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01]
     p_arr = [[p1, p2] for p1 in p_vals for p2 in p_vals]
@@ -278,11 +280,11 @@ def rapor_kitle():
     pareto_array = pareto_array[~nan_rows]
     # Revenue ekle
     combined_p = META_AUDIENCE_SIZE * pareto_array[:, 0] + IRONSOURCE_AUDIENCE_SIZE * pareto_array[:, 1]
-    pareto_df = pd.DataFrame({'P': combined_p.astype(int), 'T': pareto_array[:, 2], 'B': pareto_array[:, 3], 'Bid Meta': pareto_array[:, 4], 'Bid Ironsource': pareto_array[:, 5]})
+    pareto_df = pd.DataFrame({'P': combined_p.astype(int), 'T': pareto_array[:, 2], 'B': np.around(pareto_array[:, 3], decimals=2), 'Bid Meta': np.around(pareto_array[:, 4], decimals=2), 'Bid Ironsource': np.around(pareto_array[:, 5], decimals=2), 'P Meta': pareto_array[:, 0], 'P Ironsource': pareto_array[:, 1]})
     pareto_df = pareto_df.dropna()
     graphJSON2 = plot_pareto(pareto_df) 
 
-    return render_template('rapor_kitle.html', graphJSON2=graphJSON2, effJSON1=effJSON1, effJSON2=effJSON2, kitle=kitle)
+    return render_template('rapor_kitle.html', graphJSON2=graphJSON2, effJSON1=effJSON1, effJSON2=effJSON2, kitle=kitle, m=m, u=u)
 
 @app.route('/kampanya.html', methods =['GET', 'POST'])
 def kampanya():
@@ -297,7 +299,7 @@ def kampanya():
     if day == "1":
         df = pd.read_pickle('./df.pickle')
 
-        flash('Eski Bid: ' + str(bid))
+        flash('Eski Günlük Bütçe: ' + "{:.2f}".format(bid))
          
         kampanya_df = pd.DataFrame(columns=['Day', 'B', 'T', 'Bid', 'P', 'M', 'U', 'Reach'])
         df2 = df.copy()
@@ -308,7 +310,7 @@ def kampanya():
         kampanya_df.to_pickle("./kampanya_df.pickle")
 
         flash('Kampanya Günü: ' + str(kampanya_df.iloc[-1]['T']))
-        flash('Kalan Bütçe: ' + str(kampanya_df.iloc[-1]['B']))
+        flash('Kalan Toplam Bütçe: ' + "{:.2f}".format(kampanya_df.iloc[-1]['B']))
         flash('Sonuç: ' + str(kampanya_df.iloc[-1]['Reach'] * AUDIENCE_SIZE))
              
     kampanya_df = pd.read_pickle("./kampanya_df.pickle")
@@ -339,7 +341,7 @@ def kampanya():
         [m, u] = popt
 
         yeni_bid = new_bid(kampanya_df.iloc[-1]['P'], WORD_OF_MOUTH, m, u, kampanya_df.iloc[-1]['T'], kampanya_df.iloc[-1]['Reach'])
-        flash('Yeni bid: ' + str(yeni_bid))
+        flash('Yeni günlük bütçe: ' + "{:.2f}".format(yeni_bid))
         if yeni_bid < 0:
              flash('Amaca ulaşıldı!')
 
@@ -348,7 +350,7 @@ def kampanya():
             flash('Bütçe aşıldı!')
 
         if yeni_bid > yeni_b:
-            flash('Yeni ihale değeri, kalan bütçeyi aşıyor!')
+            flash('Yeni günlük bütçe, kalan bütçeyi aşıyor!')
 
         if kampanya_df.iloc[-1]['T'] == 1:
             flash('Kampanya süresi doldu!')
@@ -356,7 +358,7 @@ def kampanya():
         kampanya_df = kampanya_df.append(pd.DataFrame({'Day': kampanya_df.iloc[-1]['Day'] + 1, 'B': yeni_b, 'T':  kampanya_df.iloc[-1]['T']-1, 'Bid': yeni_bid, 'P':  kampanya_df.iloc[-1]['P'], 'M': m, 'U': u, 'Reach': kampanya_df.iloc[-1]['Reach'] + (sonuc / AUDIENCE_SIZE)}, index=[0]), ignore_index=True)        
         
         flash('Kampanya Günü: ' + str(kampanya_df.iloc[-1]['T']))
-        flash('Kalan Bütçe: ' + str(kampanya_df.iloc[-1]['B']))
+        flash('Kalan Toplam Bütçe: ' + "{:.2f}".format(kampanya_df.iloc[-1]['B']))
         flash('Sonuç: ' + str(kampanya_df.iloc[-1]['Reach'] * AUDIENCE_SIZE))
         
         kampanya_df.to_pickle("./kampanya_df.pickle")
@@ -372,9 +374,92 @@ def kampanya_platform():
 
 @app.route('/kampanya_kitle.html', methods =['GET', 'POST'])
 def kampanya_kitle():
-    return
+    day = request.args.get('day')
+    t = request.args.get('t')
+    b = request.args.get('b')
+    bid_kitle1 = request.args.get('bid_meta')
+    bid_kitle2 = request.args.get('bid_iron')
+    p_kitle1 = request.args.get('p_meta')
+    p_kitle2 = request.args.get('p_iron')
+    m = request.args.get('m')
+    u = request.args.get('u')
+
+    audiences = ['Kitle1', 'Kitle2']
+    audience_dict = {'Kitle1': META_AUDIENCE_SIZE, 'Kitle2': IRONSOURCE_AUDIENCE_SIZE}
+    bid_dict = {'Kitle1': bid_kitle1, 'Kitle2': bid_kitle2}
+    p_dict = {'Kitle1': p_kitle1, 'Kitle2': p_kitle2}
+    file_dict = {'Kitle1': 'Meta', 'Kitle2': 'Ironsource'}
+
+    if day == "1":
+        m = m.split(',')
+        u = u.split(',')
+        for idx, i in enumerate(audiences):
+            df = pd.read_pickle('./{}.pickle'.format(file_dict[i]))
+            flash('{} Eski Günlük Bütçe: '.format(i) + str(bid_dict[i]))
+            
+            kampanya_df = pd.DataFrame(columns=['Day', 'B', 'T', 'Bid', 'P', 'M', 'U', 'Reach'])
+            df2 = df.copy()
+            for r in range(len(df)):
+                new_entry = pd.DataFrame({'Date': df2['Date'].iloc[r], 'Day': 0, 'B': 0, 'T': 0, 'Bid': df2['Cost'].iloc[r], 'P': 0, 'M': 0, 'U': 0, 'Reach':  df2['Install'].iloc[r] / audience_dict[i]}, index=[0])            
+                kampanya_df = kampanya_df.append(new_entry, ignore_index=True)
+            kampanya_df = kampanya_df.append(pd.DataFrame({'Day': int(day), 'B': float(b), 'T': int(t), 'Bid': float(bid_dict[i]), 'P': float(p_dict[i]), 'M': float(m[idx]), 'U':float(u[idx]), 'Reach':0},  index=[0]), ignore_index=True)
+            kampanya_df.to_pickle("./kampanya_{}.pickle".format(i))
+
+        flash('Kampanya Günü: ' + str(kampanya_df.iloc[-1]['T']))
+        flash('Kalan Toplam Bütçe: ' + str(kampanya_df.iloc[-1]['B']))
+
+    campaign_dfs = [pd.read_pickle("./kampanya_{}.pickle".format(i)) for i in audiences]
+    new_bids = [0, 0]
+
+    if request.method == 'POST' and request.form['Kitle 1 Dün Harcanılan Para'] and request.form['Kitle 1 Dün Alınan Sonuç'] and request.form['Kitle 2 Dün Harcanılan Para'] and request.form['Kitle 2 Dün Alınan Sonuç']:
+        
+        actual_expenditures = [float(request.form['Kitle 1 Dün Harcanılan Para']), float(request.form['Kitle 2 Dün Harcanılan Para'])] 
+        results = [float(request.form['Kitle 1 Dün Alınan Sonuç']), float(request.form['Kitle 2 Dün Alınan Sonuç'])] 
+
+        new_entries = [pd.DataFrame({'Day': campaign_dfs[idx].iloc[-1]['Day'] + 1, 'B': 0, 'T': 0, 'Bid': actual_expenditures[idx], 'P': 0, 'M': 0, 'U': 0, 'Reach':  results[idx] / audience_dict[aud]}, index=[0]) for idx, aud in enumerate(audiences)]
+        
+        campaign_dfs = [i.sort_values(by=['Date']) for i in campaign_dfs]
+        temp_dfs = [campaign_dfs[i].append(new_entries[i], ignore_index=True) for i in range(len(audiences))]
+
+        new_budget = campaign_dfs[0].iloc[-1]['B'] - sum(actual_expenditures) # Step 2
+
+        if new_budget < 0:
+            flash('Bütçe aşıldı!') # Out of money
+
+        if campaign_dfs[0].iloc[-1]['T'] == 1:
+            flash('Kampanya süresi doldu!') # Out of time
+
+        for idx, df in enumerate(temp_dfs):
+            df['Weights'] = np.array([np.power(DECAY_RATE, i) for i in range(len(df))]) # Exponential weighting, n>4000 underflow issues
+
+            df['Verilen İhale Değeri'] = df['Bid']
+            df['Sonuç Yüzdesi'] = df['Reach']
+
+            df = df.sort_values(by=['Bid'])
+            cum_ad_spend = np.array(df['Verilen İhale Değeri'], dtype='f')
+            cum_result = np.array(df['Sonuç Yüzdesi'], dtype='f')
+            
+            p0 = [campaign_dfs[idx].iloc[-1]['M'], campaign_dfs[idx].iloc[-1]['U']]
+
+            popt, _ = curve_fit(exponential_effectiveness, cum_ad_spend, cum_result, p0=p0, maxfev=5000, sigma=df['Weights'].to_numpy())
+            [m, u] = popt
+
+            new_bids[idx] = new_bid(campaign_dfs[idx].iloc[-1]['P'], WORD_OF_MOUTH, m, u, campaign_dfs[idx].iloc[-1]['T'], campaign_dfs[idx].iloc[-1]['Reach'])
+            flash('{} Yeni bid: '.format(audiences[idx]) + "{:.2f}".format(new_bids[idx]))
+
+            campaign_dfs[idx] = campaign_dfs[idx].append(pd.DataFrame({'Day': campaign_dfs[idx].iloc[-1]['Day'] + 1, 'B': new_budget, 'T':  campaign_dfs[idx].iloc[-1]['T']-1, 'Bid': new_bids[idx], 'P':  campaign_dfs[idx].iloc[-1]['P'], 'M': m, 'U': u, 'Reach': campaign_dfs[idx].iloc[-1]['Reach'] + (results[idx] / audience_dict[audiences[idx]])}, index=[0]), ignore_index=True)        
+            campaign_dfs[idx].to_pickle("./kampanya_{}.pickle".format(audiences[idx]))
+            print(campaign_dfs[idx])
+        flash('Kampanya Günü: ' + str(campaign_dfs[0].iloc[-1]['T']))
+        flash('Kalan Toplam Bütçe: ' + "{:.2f}".format(campaign_dfs[0].iloc[-1]['B']))
+        #flash('Sonuç: ' + str(sum(df.iloc[-1]['Reach'] * AUDIENCE_SIZE))
+        
+    elif request.method == 'POST':
+        flash('Lütfen formu doldurun')
+
+    return render_template('kampanya_kitle.html', kitle1_bid=new_bids[0], kitle2_bid=new_bids[1])
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
     
     
